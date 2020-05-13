543fed1569251681bf6c2f73ee54ae3e35fb4fbd
stornig <65274770+stornig@users.noreply.github.com>
2020-05-14 00:36:17 +0500

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•SAQIB - UR -REHMAN------•◈•
+|__\~~) •||•SAQIB - KHAN---------------•◈•
+|__(-----\  •◈•------ROHIM-KHAN--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92SAQIB UR REHMAN
+\033[1;96mInstagram \033[1;93m: \033[1;FlowSAQIB
+\033[1;96mFacebook  \033[1;93m: \033[1; SAQIB
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/mr/saqib
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92SAQIB KHAN
+\033[1;96mInstagram \033[1;93m: \033[1;92SAQIB KHAN
+\033[1;96mFacebook  \033[1;93m: \033[1;92m SAQIB KHAN
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/SAQIB UR REHMAN
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "saqib"
+CorrectPassword = "saqib"
 
 loop = 'true'
 while (loop == 'true'):
